2eb5c48cac6bbb253c166d584b521d882e9613cc
Keshaini <yogeswarankeshaini@gmail.com>
2025-10-10 09:23:02 +0530

    new changes in backend

--- Backend/Models/HealthLog.js
+++ Backend/Models/HealthLog.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
 const healthLogSchema = new mongoose.Schema({
-    logID: { type: String, required: true, unique: true},
-    userID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    log_date: { type: Date, default: Date.now },
-    blood_pressure: { type: String },
-    heart_rate: { type: Number },
-    weight: { type: Number },
-    symptoms: { type: String },
-    notes: { type: String }
-});
+  userID: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", 
+    required: false 
+  },
+  log_date: { type: Date, default: Date.now },
+  blood_pressure: { type: String },
+  heart_rate: { type: Number },
+  weight: { type: Number },
+  symptoms: { type: String },
+  notes: { type: String }
+}, { timestamps: true });
 
 module.exports = mongoose.model("HealthLog", healthLogSchema);
